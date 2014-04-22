0e63c210fca465d1647ffffc99fc6d72ace85cab
Warren Togami <wtogami@gmail.com>
2014-04-21 20:22:13 -1000

    Upgrade deps to match Linux/Windows, -r1 version on intermediate tarballs, qt5

--- download.sh
+++ download.sh
@@ -20,12 +20,12 @@ ${RETRIEVE} http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
 ${RETRIEVE} http://www.opensource.apple.com/tarballs/dyld/dyld-195.5.tar.gz
 ${RETRIEVE} http://sourceforge.net/projects/e2fsprogs/files/e2fsprogs/1.41.14/e2fsprogs-libs-1.41.14.tar.gz
 ${RETRIEVE} http://www.opensource.apple.com/tarballs/ld64/ld64-127.2.tar.gz
-${RETRIEVE} http://sourceforge.net/projects/libpng/files/libpng15/older-releases/1.5.9/libpng-1.5.9.tar.gz
+${RETRIEVE} ftp://ftp.simplesystems.org/pub/png/src/history/libpng16/libpng-1.6.8.tar.gz
 ${RETRIEVE} http://opensource.apple.com/tarballs/llvmgcc42/llvmgcc42-2336.1.tar.gz
-${RETRIEVE} http://miniupnp.free.fr/files/miniupnpc-1.6.tar.gz
-${RETRIEVE} https://www.openssl.org/source/openssl-1.0.1e.tar.gz
+${RETRIEVE} http://miniupnp.free.fr/files/miniupnpc-1.9.tar.gz
+${RETRIEVE} https://www.openssl.org/source/openssl-1.0.1g.tar.gz
 ${RETRIEVE} https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2
-${RETRIEVE} http://fukuchi.org/works/qrencode/qrencode-3.2.0.tar.bz2
+${RETRIEVE} https://fukuchi.org/works/qrencode/qrencode-3.4.3.tar.bz2
 ${RETRIEVE} http://download.qt-project.org/official_releases/qt/5.2/5.2.1/single/qt-everywhere-opensource-src-5.2.1.tar.gz
 ${RETRIEVE} https://xar.googlecode.com/files/xar-1.5.2.tar.gz
 ${RETRIEVE} http://llvm.org/releases/3.2/clang+llvm-3.2-x86-linux-ubuntu-12.04.tar.gz
@@ -43,12 +43,12 @@ echo "b9d57a88f9514fa1f327a1a703756d0c1c960f4c58494a5bd80313245d13ffff  clang-ll
 echo "2cf0484c87cf79b606b351a7055a247dae84093ae92c747a74e0cde2c8c8f83c  dyld-195.5.tar.gz" | sha256sum -c
 echo "dbc7a138a3218d9b80a0626b5b692d76934d6746d8cbb762751be33785d8d9f5  e2fsprogs-libs-1.41.14.tar.gz" | sha256sum -c
 echo "97b75547b2bd761306ab3e15ae297f01e7ab9760b922bc657f4ef72e4e052142  ld64-127.2.tar.gz" | sha256sum -c
-echo "b75dae26151f9b031062c8d2f577a094b08da0ae44fe8c11175d0b9ff434cc02  libpng-1.5.9.tar.gz" | sha256sum -c
+echo "32c7acf1608b9c8b71b743b9780adb7a7b347563dbfb4a5263761056da44cc96  libpng-1.6.8.tar.gz" | sha256sum -c
 echo "1b7da383e6c1f07818d9646118ab1cc5d4498f971e9ac21e2e3d47c39ec3a713  llvmgcc42-2336.1.tar.gz" | sha256sum -c
-echo "bbd6b756e6af44b5a5b0f9b93eada3fb8922ed1d6451b7d6f184d0ae0c813994  miniupnpc-1.6.tar.gz" | sha256sum -c
-echo "f74f15e8c8ff11aa3d5bb5f276d202ec18d7246e95f961db76054199c69c1ae3  openssl-1.0.1e.tar.gz" | sha256sum -c
+echo "2923e453e880bb949e3d4da9f83dd3cb6f08946d35de0b864d0339cf70934464  miniupnpc-1.9.tar.gz" | sha256sum -c
+echo "53cb818c3b90e507a8348f4f5eaedb05d8bfe5358aabb508b7263cc670c3e028  openssl-1.0.1g.tar.gz" | sha256sum -c
 echo "13bfc5ae543cf3aa180ac2485c0bc89495e3ae711fc6fab4f8ffe90dfb4bb677  protobuf-2.5.0.tar.bz2" | sha256sum -c
-echo "03c4bc7cd9a75747c3815d509bbe061907d615764f2357923f0db948c567068f  qrencode-3.2.0.tar.bz2" | sha256sum -c
+echo "dfd71487513c871bad485806bfd1fdb304dedc84d2b01a8fb8e0940b50597a98  qrencode-3.4.3.tar.bz2" | sha256sum -c
 echo "84e924181d4ad6db00239d87250cc89868484a14841f77fb85ab1f1dbdcd7da1  qt-everywhere-opensource-src-5.2.1.tar.gz" | sha256sum -c
 echo "4c5d5682803cdfab16d72365cf51fc4075d597c5eeaa8c7d1990fea98cdae3e6  xar-1.5.2.tar.gz" | sha256sum -c
 echo "d1c030756ecc182defee9fe885638c1785d35a2c2a297b4604c0e0dcc78e47da  cdrkit-1.1.11.tar.gz" | sha256sum -c
--- native-openssl.mk
+++ native-openssl.mk
@@ -1,9 +1,9 @@
 include native-include.mk
-SOURCE_FILE=$(SOURCES_PATH)/openssl-1.0.1e.tar.gz
-BUILD_DIR=$(BUILD_BASE)/native-openssl-1.0.1e
+SOURCE_FILE=$(SOURCES_PATH)/openssl-1.0.1g.tar.gz
+BUILD_DIR=$(BUILD_BASE)/native-openssl-1.0.1g
 
-$(NATIVEPREFIX)/lib/libssl.a: $(SOURCES_PATH)/openssl-1.0.1e.tar.gz
-	tar -C $(BUILD_BASE) -xf $<; mv $(BUILD_BASE)/openssl-1.0.1e $(BUILD_DIR)
+$(NATIVEPREFIX)/lib/libssl.a: $(SOURCES_PATH)/openssl-1.0.1g.tar.gz
+	tar -C $(BUILD_BASE) -xf $<; mv $(BUILD_BASE)/openssl-1.0.1g $(BUILD_DIR)
 	cd $(BUILD_DIR); ./Configure --prefix=$(NATIVEPREFIX) -no-shared -no-zlib-dynamic -no-test linux-generic32
 	sed -ie "/define DATE/d" $(BUILD_DIR)/crypto/Makefile
 	cd $(BUILD_DIR); $(MAKE) -j1 CC="$(CC) -m32" depend
