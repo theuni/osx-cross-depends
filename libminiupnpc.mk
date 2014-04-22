a01caf4e9e6b12a0e736e17f670b78e5a8965cea
Warren Togami <wtogami@gmail.com>
2014-04-22 12:16:05 -1000

    Fix build of miniupnpc-1.9 on OSX 10.6

--- libminiupnpc.mk
+++ libminiupnpc.mk
@@ -4,6 +4,7 @@ BUILD_DIR=$(BUILD_BASE)/miniupnpc-1.9
 
 $(PREFIX)/lib/libminiupnpc.a: $(SOURCE_FILE)
 	tar -C $(BUILD_BASE) -xf $<
+	cd $(BUILD_DIR); patch -p1 < $(PATCHES_DIR)/miniupnpc/miniupnc-1.9-osx-10.6-inaddr_none_hack.patch
 	sed -ie "s|IP_MULTICAST_IF|32|" $(BUILD_DIR)/miniupnpc.c
 	cd $(BUILD_DIR); $(MAKE) CC=$(CC) AR=$(AR) CFLAGS="$(CFLAGS) $(CPPFLAGS)" libminiupnpc.a
 	cd $(BUILD_DIR); install -d $(PREFIX)/include/miniupnpc
