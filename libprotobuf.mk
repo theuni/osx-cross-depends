121577d6c319fd214a2b0fdb121b09d739b900f6
Cory Fields <cory@coryfields.com>
2014-05-07 13:14:53 -0400

    libprotobuf: only build/install the lib

--- libprotobuf.mk
+++ libprotobuf.mk
@@ -5,6 +5,7 @@ BUILD_DIR=$(BUILD_BASE)/protobuf-2.5.0
 $(PREFIX)/lib/libprotobuf.a: $(SOURCE_FILE)
 	tar -C $(BUILD_BASE) -xjf $<
 	cd $(BUILD_DIR); $(CONFIGURE) --enable-shared=no --disable-dependency-tracking --with-protoc=$(NATIVEPREFIX)/bin/protoc
-	cd $(BUILD_DIR); $(MAKE)
-	cd $(BUILD_DIR); $(MAKE) install
+	cd $(BUILD_DIR)/src; $(MAKE) libprotobuf.la
+	cd $(BUILD_DIR)/src; $(MAKE) install-libLTLIBRARIES install-nobase_includeHEADERS
+	cd $(BUILD_DIR); $(MAKE) install-pkgconfigDATA
 	touch $@
