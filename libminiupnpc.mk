83e71a6b31cdcb42576623640bf63d52242c93a9
Cory Fields <cory@coryfields.com>
2014-05-07 11:41:01 -0400

    miniupnpc: Remove hacks. Clean fix.

--- libminiupnpc.mk
+++ libminiupnpc.mk
@@ -4,8 +4,7 @@ BUILD_DIR=$(BUILD_BASE)/miniupnpc-1.9
 
 $(PREFIX)/lib/libminiupnpc.a: $(SOURCE_FILE)
 	tar -C $(BUILD_BASE) -xf $<
-	sed -ie "s|IP_MULTICAST_IF|32|" $(BUILD_DIR)/miniupnpc.c
-	cd $(BUILD_DIR); $(MAKE) CC=$(CC) AR=$(AR) CFLAGS="$(CFLAGS) $(CPPFLAGS)" libminiupnpc.a
+	cd $(BUILD_DIR); CFLAGS="$(CFLAGS) $(CPPFLAGS)" $(MAKE) OS=Darwin CC=$(CC) AR=$(AR) libminiupnpc.a
 	cd $(BUILD_DIR); install -d $(PREFIX)/include/miniupnpc
 	cd $(BUILD_DIR); install *.h $(PREFIX)/include/miniupnpc
 	cd $(BUILD_DIR); install libminiupnpc.a  $(PREFIX)/lib
