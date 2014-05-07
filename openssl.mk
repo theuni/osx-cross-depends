e8ecb4ee65fed130fb62dbe9ec8357468feecdc9
Cory Fields <cory@coryfields.com>
2014-05-07 13:15:43 -0400

    openssl: properly filter for x86 or x86_64

--- openssl.mk
+++ openssl.mk
@@ -1,6 +1,8 @@
 include include.mk
 SOURCE_FILE=$(SOURCES_PATH)/openssl-1.0.1g.tar.gz
 BUILD_DIR=$(BUILD_BASE)/openssl-1.0.1g
+ARCH=$(findstring x86_64,$(HOST))$(subst i686,i386,$(findstring i686,$(HOST)))
+OPENSSL_ARCH=$(subst darwin-x86_64-cc, darwin64-x86_64-cc, darwin-$(ARCH)-cc)
 
 $(PREFIX)/lib/libssl.a: $(SOURCE_FILE)
 	tar -C $(BUILD_BASE) -xf $<
@@ -10,7 +12,7 @@ $(PREFIX)/lib/libssl.a: $(SOURCE_FILE)
 	cd $(BUILD_DIR); patch -p0 < $(PATCHES_DIR)/openssl/patch-Makefile.org.diff
 	sed -ie "s|cc:|$(CC):|" $(BUILD_DIR)/Configure
 	sed -ie "s|\(-arch [_a-zA-Z0-9]*\)|\1 --sysroot $(SDK)  -target $(HOST) -msse2|" $(BUILD_DIR)/Configure
-	cd $(BUILD_DIR); AR=$(AR) RANLIB=$(RANLIB) ./Configure -L$(PREFIX)/lib --openssldir=$(PREFIX)/etc/openssl zlib no-krb5 no-shared darwin-i386-cc $(CPPFLAGS) --prefix=$(PREFIX)
+	cd $(BUILD_DIR); AR=$(AR) RANLIB=$(RANLIB) ./Configure -L$(PREFIX)/lib --openssldir=$(PREFIX)/etc/openssl zlib no-krb5 no-shared $(OPENSSL_ARCH) $(CPPFLAGS) --prefix=$(PREFIX)
 	sed -i "s|engines apps test|engines|" $(BUILD_DIR)/Makefile
 	sed -i "/define DATE/d" $(BUILD_DIR)/crypto/Makefile
 	cd $(BUILD_DIR); $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
