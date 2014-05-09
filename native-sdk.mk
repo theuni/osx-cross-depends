99e8a4cc16920ef50e99f7cd9a17d36890921b92
Cory Fields <cory@coryfields.com>
2014-05-09 10:59:56 -0400

    sdk: use 7z rather than building xar

--- native-sdk.mk
+++ native-sdk.mk
@@ -3,7 +3,7 @@ SOURCE_FILE=$(SOURCES_PATH)/MacOSX10.6.pkg
 
 $(SDK)/SDKSettings.plist: $(SOURCE_FILE) $(NATIVEPREFIX)/bin/xar
 	mkdir -p $(@D)
-	cd SDKs; $(NATIVEPREFIX)/bin/xar -x -f $(SOURCE_FILE)
+	cd SDKs; 7z -y e $(SOURCE_FILE)
 	gunzip -c SDKs/Payload | cpio -i
 	cd SDKs; rm -f Bom PackageInfo Payload
 	touch $@
